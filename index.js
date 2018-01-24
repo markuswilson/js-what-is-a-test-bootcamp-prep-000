5817360081ee39252d5960d901786b49e960ace6
Mark Wilson <markus_wilson@msn.com>
2018-01-24 05:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
-var message = `${name} is the name and ${height} is the height`
 module.exports = {name, height, message}
 
 console.log(message)
